4a1d1af296d687fe34b8696900498fab68357263
diegomvh <diegomvh@gmail.com>
2012-11-19 09:16:15 -0300

    ipython widget build kernel

--- pmxpy/ipython.py
+++ pmxpy/ipython.py
@@ -18,13 +18,14 @@ class IPythonDock(QtGui.QDockWidget, PMXBaseDock):
         PMXBaseDock.__init__(self)
         self.setWindowTitle(_("IPython"))
         self.setObjectName(_("IPythonDock"))
+        self.kernelManager, self.connection = self.buildKernelManager()
         self.setupConsole()
 
     def setupConsole(self):
         try:
             from IPython.frontend.qt.console.ipython_widget import IPythonWidget
             self.console = IPythonWidget()
-            self.console.kernel_manager = self.application.kernelManager
+            self.console.kernel_manager = self.kernelManager
             self.console.set_default_style(colors="linux")
         except ImportError:
             # Gracefuly fail if iPython is not available
@@ -34,3 +35,30 @@ class IPythonDock(QtGui.QDockWidget, PMXBaseDock):
             tb = format_exc()
             self.console.appendPlainText("IPython console disabled because of\n%s\nPlese install ipython >= 0.11" % tb)
         self.setWidget(self.console)
+
+    def buildKernelManager(self):
+        kernelManager = None
+        try:
+            from IPython.frontend.qt.kernelmanager import QtKernelManager
+            kernelManager = QtKernelManager()
+            kernelManager.start_kernel()
+            kernelManager.start_channels()
+            if hasattr(kernelManager, "connection_file"):
+                ipconnection = kernelManager.connection_file
+            else:
+                shell_port = kernelManager.shell_address[1]
+                iopub_port = kernelManager.sub_address[1]
+                stdin_port = kernelManager.stdin_address[1]
+                hb_port = kernelManager.hb_address[1]
+                ipconnection = "--shell={0} --iopub={1} --stdin={2} --hb={3}".format(shell_port, iopub_port, stdin_port, hb_port)
+        except ImportError as e:
+            self.logger.warn("Warning: %s" % e)
+            ipconnection = kernelManager = None
+        return kernelManager, ipconnection
+        
+    def environmentVariables(self):
+        env = {}
+        if self.connection:
+            env["PMX_IPYTHON_CONNECTION"] = self.connection
+            env["PMX_IPYTHON_KERNEL"] = self.connection
+        return env
