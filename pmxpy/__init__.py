e7dbc270609c37655c05a36932a6f94e8ef52318
diegomvh <diegomvh@gmail.com>
2014-04-07 18:16:56 -0300

    Descriptor arg

--- pmxpy/__init__.py
+++ pmxpy/__init__.py
@@ -6,6 +6,6 @@ from prymatex.gui.codeeditor.editor import CodeEditor
 from pmxpy.ipython import IPythonDock
 from pmxpy.addons import PythonCheckerAddon
 
-def registerPlugin(manager):
+def registerPlugin(manager, descriptor):
     manager.registerComponent(IPythonDock)
     manager.registerComponent(PythonCheckerAddon, CodeEditor)
